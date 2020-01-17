cc382d3243604bbfdc819e4f450157086bff5de9
Naga Sai Katakamsetty <awebdesigner09@gmail.com>
2020-01-17 06:55:22 +0530

    Search results

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,8 @@ namespace OrigTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace OrigTest
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
 
             app.UseRouting();
 
